76440d5b6d42fb53a934c18a4f8ffb8b8a0738bb
chenhailong <chenhailong@192.168.31.187>
2020-05-15 00:24:24 +0800

    add gits

--- src/main/java/com/chl/spi/SPITest.java
+++ src/main/java/com/chl/spi/SPITest.java
@@ -2,11 +2,17 @@ package com.chl.spi;
 
 import java.util.ServiceLoader;
 
+/**
+ * 这样处理好像没法直接成功。
+ * @author chenhailong
+ *
+ */
 public class SPITest {
 
 	public static void main(String[] args) {
 
 		ServiceLoader<Info> infos = ServiceLoader.load(Info.class);
+
 		for(Info inf : infos) {
 			System.out.println("--");
 			inf.laugh();
