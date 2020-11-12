6ddecef3df1cd92d0f6cbe919dc46df8535e5fc1
chl <chenhailong@mistong.com>
2020-11-12 11:07:04 +0800

    处理信息

--- src/main/java/com/chl/dataStruct/map/TestHashMap.java
+++ src/main/java/com/chl/dataStruct/map/TestHashMap.java
@@ -2,18 +2,32 @@ package com.chl.dataStruct.map;
 
 import java.util.HashMap;
 
+import com.alibaba.fastjson.JSON;
+
 public class TestHashMap {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		HashMap<String,Object> hm = new HashMap<String,Object>(2);
 		
-		hm.put("abc", "abc");
-		hm.put("bcd", "bcd");
+		hm.put("734449887340363776", 1);
+		hm.put("742720327904821248", 1);
+		hm.put("742799312353394688", 0);
+		hm.put("742720327904821248", 0);
+		hm.put("742720327904821248", 0);
+		hm.put("742720327904821248", 1);
+		hm.put("743239374702542848", 0);
 		
-		System.out.println(hm.containsKey("bbc"));
+		System.out.println(JSON.toJSON(hm));
 		
-		h
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 
---
+++ src/main/java/com/chl/string/TestStringBuffer.java
@@ -0,0 +1,15 @@
+package com.chl.string;
+
+public class TestStringBuffer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sb = new StringBuffer("");
+		sb.append(" a");
+		sb.append(" c");
+		sb.append(" c");
+		
+		System.out.println(sb.toString());
+	}
+
+}
